bfa8d73fec91b61c326d72cddb4637af91f2c119
jbill2000 <83095049+jbill2000@users.noreply.github.com>
2021-09-20 11:15:42 -0700

    Add files via upload

--- NetworkClient.java
+++ NetworkClient.java
@@ -3,45 +3,59 @@ import java.io.*;
 import java.net.*;
 public class NetworkClient {
     public static void main(String[] args) throws IOException  {
-
+        //Try catch to catch there
+        try{
+            //variables
         int portnumber = 3900;
         DataInputStream input;
         boolean valid= true;
         DataOutputStream output;
         Socket theClient;
         char theOperator;
+                //scanner and socket creation
                 Scanner scan= new Scanner(System.in);
                 theClient = new Socket("127.0.0.1", portnumber);
-                System.out.println("Client Connected!");
-                output= new DataOutputStream(theClient.getOutputStream());
-                input= new DataInputStream(theClient.getInputStream());
-                System.out.println("Please enter an operator. Either + - * or /");
-                String operator=scan.next();
-                if(!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/"))
-                {
-                    valid=false;
-                    while(valid==false) {
 
 
-                        System.out.println("Operator wasn't valid. Please enter a new operator");
-                        operator=scan.next();
-                        //theOperator=operator.charAt(0);
-                        if(operator.equals("+")||operator.equals("-")||operator.equals("*")||operator.equals("/"))
-                        {
-                            System.out.println("Valid Operator. Thank you");
-                            valid=true;
-                        }
+                    //print to print the client connected
+                   System.out.println("Client Connected!");
+                   //input and output stream initialization
+                   output = new DataOutputStream(theClient.getOutputStream());
+                   input = new DataInputStream(theClient.getInputStream());
+                   //asks for an operator and reads it in
+                   System.out.println("Please enter an operator. Either + - * or /");
+                   String operator = scan.next();
+                   //if operator is invalid keep looping
+                   if (!operator.equals("+") && !operator.equals("-") && !operator.equals("*") && !operator.equals("/")) {
+                       valid = false;
+                       while (valid == false) {
+
+                            //keep reading an operator till its valid
+                           System.out.println("Operator wasn't valid. Please enter a new operator");
+                           operator = scan.next();
+                           //Checks if operator is valid and quits the loop
+                           if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                               System.out.println("Valid Operator. Thank you");
+                               valid = true;
+                           }
 
 
-                    }
+                       }
 
-                }
-                output.writeChars(operator);
-                //Now client will receive results from server
+                   }
+                   //sends the operator to the server
+                   output.writeChars(operator);
+                   //Now client will receive results from server
 
-               int result= input.readInt();
-                System.out.println("The Result is "+result);
+                   int result = input.readInt();
+                   //prints the result
+                   System.out.println("The Result is " + result);
+                   //tells client to start server first
+               }catch(ConnectException ce)
+               {
+                   System.out.println("Connection Refused. Please Start the server then the client.");
 
+               }
 
 
 
--- NetworkServer.java
+++ NetworkServer.java
@@ -1,36 +1,37 @@
-import java.nio.channels.ScatteringByteChannel;
 import java.util.*;
 import java.io.*;
 import java.net.*;
 public class NetworkServer {
 
     public static void main(String[] args) throws IOException {
-
+        //variables
         int portnumber= 3900;
         Scanner scan= new Scanner(System.in);
         DataInputStream input;
         DataOutputStream output;
         ServerSocket listener;
-
          int num1=0;
          int num2=0;
         int result=0;
         boolean check=false;
         char incoming='a';
+            //initalization for listener socket and data streams as well as printing server was connected
             listener = new ServerSocket(portnumber);
             Socket servsocket= listener.accept();
             System.out.println("Server Connected");
             output= new DataOutputStream(servsocket.getOutputStream());
             input= new DataInputStream(servsocket.getInputStream());
+            //reads in the operator from the client
             incoming = input.readChar();
-
+            //tells user to enter two numbers
              System.out.println("Please enter two numbers followed by a space");
+             //keeps looping until the numbers are valid
             do {
                 try {
                         check=true;
                     num1 = scan.nextInt();
                     num2 = scan.nextInt();
-
+                        //accounts for divide by 0 errors
                     if (incoming == '/' && num2 == 0) {
                         while (num2 == 0) {
                             System.out.println("Can't Divide by 0. Please input two new numbers");
@@ -39,14 +40,15 @@ public class NetworkServer {
                         }
 
                     }
-
+                    //catches anything other than a number
                 } catch (InputMismatchException ie) {
                     System.out.println("Input was not a number");
                     check=false;
                     scan.nextLine();
                 }
-            }while(check!=true);
 
+            }while(check!=true);
+                //switch case to figure out which operation to do with the operator the user passed in
                 switch(incoming) {
                     case '+': {
                         result = (num1 + num2);
@@ -65,7 +67,9 @@ public class NetworkServer {
                         break;
                     }
                 }
+                //writes the result to the client for printing
                 output.writeInt(result);
+                //prints the result on server side.
                 System.out.println("The Result is "+result);
 
 
